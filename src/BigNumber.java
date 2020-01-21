22e02f993440c8d37e4b5add4aa7c89ffc6e6ab0
Dani Carrillo <danielcarrillocardus@gmail.com>
2020-01-21 18:14:29 +0100

    Añadidas clase BigNumber y Tests

---
+++ src/BigNumber.java
@@ -0,0 +1,35 @@
+class BigNumber {
+    // Constructor 1
+    public BigNumber(String s) { }
+    // Constructor 2
+    public BigNumber(BigNumber b) { }
+    // Suma
+    BigNumber add(BigNumber other) {
+        return other;
+    }
+    // Resta
+    BigNumber sub(BigNumber other) {
+        return other;
+    }
+    // Multiplica
+    BigNumber mult(BigNumber other) {
+        return other;
+    }
+    // Divideix
+    BigNumber div(BigNumber other) {
+        return other;
+    }
+    // Compara dos BigNumber. Torna 0 si són iguals, -1 si el primer és menor
+// i torna 1 si el segon és menor
+    public int compareTo(BigNumber other) {
+        return 0;
+    }
+    // Torna un String representant el número
+    public String toString() {
+        return "TODO";
+    }
+    // Mira si dos objectes BigNumber són iguals
+    public boolean equals(Object other) {
+        return true;
+    }
+}
