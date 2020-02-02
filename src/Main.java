ca61686dbc892033787c98258d989c48162ada1c
unknown <danielcarrillocardus@gmail.com>
2020-02-03 00:40:55 +0100

    Final commit. Deleted main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BigNumber bn1 = new BigNumber("2754");
-        BigNumber bn2 = new BigNumber("9");
-        System.out.println(bn1.div(bn2));
-    }
-}
